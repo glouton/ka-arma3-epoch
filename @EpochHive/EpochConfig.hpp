d186be5d608e7d149471b9938e8290f920b73041
glouton <charles@g2cw2c.fr>
2015-07-10 12:22:15 +0200

    But back all our customizations but those in mission.sqm

--- @EpochHive/EpochConfig.hpp
+++ @EpochHive/EpochConfig.hpp
@@ -1,17 +1,17 @@
-serverRestart         = false; // true sends #shutdown command to server after the time specified in ForceRestartTime
-forceRestartTime      = 14400; // 4 hour restarts
+serverRestart         = true; // true sends #shutdown command to server after the time specified in ForceRestartTime
+forceRestartTime      = 21600; // 6 hour restarts
 
 StorageSlotsLimit = 1500; // Max storage slots allowed. Warning! Higher the number lower performance.
 BuildingSlotsLimit = 1500; // Max building slots allowed. Warning! Higher the number lower performance.
 
 // Time based
-StaticDateTime[] = {2035,6,10,7,0}; // {0,0,0,8,0} would forces the server to start at 8am each time it is started while allowing the year, month and day to stay real time. Any values left at 0 will result in no change.
+StaticDateTime[] = {0,0,0,3,0}; // {0,0,0,8,0} would forces the server to start at 8am each time it is started while allowing the year, month and day to stay real time. Any values left at 0 will result in no change.
 timeDifference = 0; // Server uses real time this will allow you to offset just the hour.
-timeMultiplier = 4; // Sets a time multiplier for in-game time. The command range is now capped at 0.1 - 120 to avoid performance problems.
-lootMultiplier = 0.5; // 1 = max loot bias. This controls how much loot can payout per Epoch loot container.
+timeMultiplier = 3; // Sets a time multiplier for in-game time. The command range is now capped at 0.1 - 120 to avoid performance problems.
+lootMultiplier = 0.1; // 1 = max loot bias. This controls how much loot can payout per Epoch loot container.
 // Events
-WeatherChangeTime = 1200; // This controls how fast the weather changes as well as how fast shipping containers and earthquakes happen.
-WeatherStaticForecast[] = {}; // Default: {75.5,0,{0,0,0},0,{1,1}}; // Clear day; {19,1,{1,1,40},1,{5,5}}; // Cold Foggy Rainy Overcast Windy; Format: {temp <scalar>,rain <scalar>,fog <array>,overcast <scalar>,wind <array>} 
+WeatherChangeTime = 2400; // This controls how fast the weather changes as well as how fast shipping containers and earthquakes happen.
+WeatherStaticForecast[] = {75.5,0,{0,0,0},0,{5,5}}; // Default: {75.5,0,{0,0,0},0,{1,1}}; // Clear day; {19,1,{1,1,40},1,{5,5}}; // Cold Foggy Rainy Overcast Windy; Format: {temp <scalar>,rain <scalar>,fog <array>,overcast <scalar>,wind <array>} 
 events[] = {
     { 3600, "CarnivalSpawner" }, // SECOND <scalar>, EVENT <string>
     { 1200, "EarthQuake" },
@@ -20,77 +20,101 @@ events[] = {
 };
 
 // Antagonists
-antagonistChanceTrash = 0.09; //9% chance when player loot a trash object
+antagonistChanceTrash = 0.20; //9% chance when player loot a trash object
 antagonistChancePDeath = 0.33; //33% chance when player was killed from a other player (selfkill doesn't count)
-antagonistChanceLoot = 0.09; //9% chance when player click "SEARCH" on a loot object
+antagonistChanceLoot = 0.20; //9% chance when player click "SEARCH" on a loot object
 
 // Player Related
 cloneCost = 100; // debt incurred on player death
 
 // vehicles - Max vehicle slots is calculated from per vehicle limits below. Warning! Higher the number lower the performance.
-vehicleLockTime = 1800; // Controls how many seconds it takes to allow another person/group to unlock vehicle.
+vehicleLockTime = 7200; // Controls how many seconds it takes to allow another person/group to unlock vehicle.
 allowedVehiclesList[] = {
-    {"C_Offroad_01_EPOCH",8},
-    {"C_Quadbike_01_EPOCH",8},
-    {"C_Hatchback_01_EPOCH",10},
-    {"C_Hatchback_02_EPOCH",10},
-    {"C_SUV_01_EPOCH",10},
-    {"C_Rubberboat_EPOCH",5},
-    {"C_Rubberboat_02_EPOCH",5},
-    {"C_Rubberboat_03_EPOCH",5},
-    {"C_Rubberboat_04_EPOCH",5},
-    {"C_Van_01_box_EPOCH",8},
-    {"C_Van_01_transport_EPOCH",9},
-    {"C_Boat_Civil_01_EPOCH",5},
-    {"C_Boat_Civil_01_police_EPOCH",5},
-    {"C_Boat_Civil_01_rescue_EPOCH",5},
-    {"B_Heli_Light_01_EPOCH",2},
-    {"B_SDV_01_EPOCH",2},
-    {"B_MRAP_01_EPOCH",3},
-    {"B_Truck_01_transport_EPOCH",1},
-    {"B_Truck_01_covered_EPOCH",2},
-    {"B_Truck_01_mover_EPOCH",1},
-    {"B_Truck_01_box_EPOCH",1},
-    {"O_Truck_02_covered_EPOCH",2},
-    {"O_Truck_02_transport_EPOCH",1},
-    {"O_Truck_03_covered_EPOCH",1},
-    {"O_Truck_02_box_EPOCH",1},
-    {"I_Heli_light_03_unarmed_EPOCH",1},
-    {"O_Heli_Light_02_unarmed_EPOCH",1},
-    {"I_Heli_Transport_02_EPOCH",1},
-    {"O_Heli_Transport_04_EPOCH",1},
-    {"O_Heli_Transport_04_bench_EPOCH",1},
-    {"O_Heli_Transport_04_box_EPOCH",1},
-    {"O_Heli_Transport_04_covered_EPOCH",1},
-    {"B_Heli_Transport_03_unarmed_EPOCH",1},
-    {"jetski_epoch",7},
-    {"K01",2},
-    {"K02",2},
-    {"K03",2},
-    {"K04",2},
-    {"ebike_epoch",7},
-    {"mosquito_epoch",7}
+
+// Boats
+
+{"C_Boat_Civil_01_EPOCH",6},
+{"C_Boat_Civil_01_rescue_EPOCH",6},
+{"jetski_epoch",3},
+{"B_SDV_01_EPOCH",3},
+
+// Bikes
+
+{"C_Quadbike_01_EPOCH",4},
+
+// Cars
+
+{"C_Offroad_01_EPOCH",8},
+{"C_Hatchback_01_EPOCH",8},
+{"C_Hatchback_02_EPOCH",8},
+{"C_SUV_01_EPOCH",8},
+
+// Vans
+
+{"C_Van_01_box_EPOCH",6},
+{"C_Van_01_transport_EPOCH",6},
+{"B_G_Van_01_fuel_F",3},
+{"I_Truck_02_fuel_F",3},
+
+// Military Wheeled
+
+{"B_MRAP_01_EPOCH",8},
+{"O_MRAP_02_F",8},
+{"I_MRAP_03_F",8},
+
+// Military Trucks
+
+{"B_Truck_01_transport_EPOCH",1},
+{"B_Truck_01_covered_EPOCH",1},
+{"B_Truck_01_mover_EPOCH",1},
+{"B_Truck_01_box_EPOCH",1},
+{"O_Truck_02_covered_EPOCH",1},
+{"O_Truck_02_transport_EPOCH",2},
+{"O_Truck_03_covered_EPOCH",2},
+{"O_Truck_02_box_EPOCH",2},
+{"O_Truck_03_medical_F",6},
+{"O_Truck_03_covered_F",4},
+
+// Helicopters 
+         
+{"B_Heli_Light_01_EPOCH",8},              //MH9 Hummingbird
+{"I_Heli_Transport_02_EPOCH",4},          //Mohawk
+{"O_Heli_Transport_04_EPOCH",5},          //Taru Lift
+{"O_Heli_Transport_04_bench_EPOCH",3},    //Taru Bench
+{"O_Heli_Transport_04_box_EPOCH",3},      //Taru Transport
+{"O_Heli_Transport_04_covered_EPOCH",2},  //LB Wasp
+{"B_Heli_Transport_03_unarmed_EPOCH",2},  //Huron
+{"I_Heli_light_02_unarmed_F",2},          //Orca
+{"C_Heli_light_01_sheriff_F",1},          //LB Sherrif
+{"C_Heli_light_01_furious_F",1},          //LB Furious
+{"C_Heli_light_01_digital_F",1},          //LB digital
+{"C_Heli_light_01_speedy_F",1},           //LB speedy
+{"C_Heli_light_01_wasp_F",2},             //LB Wasp
+{"mosquito_epoch",3},                     //Coffin
+{"B_Heli_Transport_01_F",4},             //Goshawk
+{"B_Heli_Transport_03_black_F",4},       //Huron Armed
+
 };
 
 // Traders
-taxRate = 0.1; // controls the price increase for purchases
-starterTraderItems[] = { { "ItemSodaBurst", "meatballs_epoch", "MortarBucket", "CinderBlocks", "VehicleRepair", "CircuitParts", "ItemCorrugated", "PartPlankPack", "ItemRock", "ItemRope", "ItemStick" }, { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 } }; // Starter Items for fresh spawned trader first array is classnames second is quantity.
-NPCSlotsLimit = 50; // Max number of traders static or dynamic. Warning! Higher the number lower performance.
-forceStaticTraders = false; // disables traders moving from work to home
+taxRate = 2.5; // controls the price increase for purchases
+starterTraderItems[] = { { "ItemSodaBurst", "meatballs_epoch", "MortarBucket", "CinderBlocks", "VehicleRepair", "CircuitParts", "ItemCorrugated", "PartPlankPack", "ItemRock", "ItemRope", "ItemStick" }, { 5, 5, 5, 10, 5, 5, 10, 5, 5, 5, 5 } }; // Starter Items for fresh spawned trader first array is classnames second is quantity.
+NPCSlotsLimit = 30; // Max number of traders static or dynamic. Warning! Higher the number lower performance.
+forceStaticTraders = true; // disables traders moving from work to home
 
 // Markers
 showEarthQuakes = true; // show mineral viens caused by earthquakes
-showShippingContainers = true; // Show location of events based loots (plants, shipping container, Carnival)
+showShippingContainers = false; // Show location of events based loots (plants, shipping container, Carnival)
 SHOW_TRADERS = true; // Show locations of traders
 SHOW_JAMMERS = false; // Shows location of base jammers 
 SHOW_BOATLOOT = true; // Shows the location of shipwreck loot 
 DEBUG_VEH = false; // DEBUG ONLY used to debug spawing of vehicles
 
 // Hive Related
-expiresBuilding = "604800";  // expiration date in seconds for buildings
+expiresBuilding = "1209600";  // expiration date in seconds for buildings
 expiresPlayer = "2592000";  // expiration date in seconds for players
 expiresBank = "7776000";  // expiration date in seconds for players bank
-expiresVehicle = "604800";  // expiration date in seconds for vehicles
+expiresVehicle = "1209600";  // expiration date in seconds for vehicles
 expiresAIdata = "604800";  // expiration date in seconds for NPC Trader inventory
 hiveAsync = true; // true = asynchronous hive calls (non blocking), false = synchronous hive calls (blocking)
 
