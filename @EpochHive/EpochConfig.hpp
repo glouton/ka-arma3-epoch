a0db3320b1866b1cdccaef15ffbb9e431fd5f9a2
glouton <charles@g2cw2c.fr>
2015-07-30 17:41:52 +0200

    Downloaded custom version of EpochConfig.hpp

--- @EpochHive/EpochConfig.hpp
+++ @EpochHive/EpochConfig.hpp
@@ -1,5 +1,5 @@
-serverRestart         = false; // true sends #shutdown command to server after the time specified in ForceRestartTime
-forceRestartTime      = 14400; // 4 hour restarts
+serverRestart         = true; // true sends #shutdown command to server after the time specified in ForceRestartTime
+forceRestartTime      = 21600; // 4 hour restarts
 
 StorageSlotsLimit = 1500; // Max storage slots allowed. Warning! Higher the number lower performance.
 BuildingSlotsLimit = 1500; // Max building slots allowed. Warning! Higher the number lower performance.
@@ -7,7 +7,7 @@ BuildingSlotsLimit = 1500; // Max building slots allowed. Warning! Higher the nu
 // Time based
 StaticDateTime[] = {2035,6,10,7,0}; // {0,0,0,8,0} would forces the server to start at 8am each time it is started while allowing the year, month and day to stay real time. Any values left at 0 will result in no change.
 timeDifference = 0; // Server uses real time this will allow you to offset just the hour.
-timeMultiplier = 4; // Sets a time multiplier for in-game time. The command range is now capped at 0.1 - 120 to avoid performance problems.
+timeMultiplier = 8; // Sets a time multiplier for in-game time. The command range is now capped at 0.1 - 120 to avoid performance problems.
 lootMultiplier = 0.5; // 1 = max loot bias. This controls how much loot can payout per Epoch loot container.
 // Events
 airDrops = false; // TBA
@@ -33,68 +33,93 @@ cloneCost = 100; // debt incurred on player death
 simulationHandler = false; // When enabled this feature disables simulation on vehicles that are not near players. Can help improve client fps at the cost of server fps. (This is disabled by default now that Arma has fixed the original issue)
 vehicleLockTime = 1800; // Controls how many seconds it takes to allow another person/group to unlock vehicle.
 allowedVehiclesList[] = {
-    {"C_Offroad_01_EPOCH",8},
-    {"C_Quadbike_01_EPOCH",8},
-    {"C_Hatchback_01_EPOCH",10},
-    {"C_Hatchback_02_EPOCH",10},
-    {"C_SUV_01_EPOCH",10},
-    {"C_Rubberboat_EPOCH",5},
-    {"C_Rubberboat_02_EPOCH",5},
-    {"C_Rubberboat_03_EPOCH",5},
-    {"C_Rubberboat_04_EPOCH",5},
-    {"C_Van_01_box_EPOCH",8},
-    {"C_Van_01_transport_EPOCH",9},
-    {"C_Boat_Civil_01_EPOCH",5},
-    {"C_Boat_Civil_01_police_EPOCH",5},
-    {"C_Boat_Civil_01_rescue_EPOCH",5},
-    {"B_Heli_Light_01_EPOCH",2},
-    {"B_SDV_01_EPOCH",2},
-    {"B_MRAP_01_EPOCH",3},
-    {"B_Truck_01_transport_EPOCH",1},
-    {"B_Truck_01_covered_EPOCH",2},
-    {"B_Truck_01_mover_EPOCH",1},
-    {"B_Truck_01_box_EPOCH",1},
-    {"O_Truck_02_covered_EPOCH",2},
-    {"O_Truck_02_transport_EPOCH",1},
-    {"O_Truck_03_covered_EPOCH",1},
-    {"O_Truck_02_box_EPOCH",1},
-    {"I_Heli_light_03_unarmed_EPOCH",1},
-    {"O_Heli_Light_02_unarmed_EPOCH",1},
-    {"I_Heli_Transport_02_EPOCH",1},
-    {"O_Heli_Transport_04_EPOCH",1},
-    {"O_Heli_Transport_04_bench_EPOCH",1},
-    {"O_Heli_Transport_04_box_EPOCH",1},
-    {"O_Heli_Transport_04_covered_EPOCH",1},
-    {"B_Heli_Transport_03_unarmed_EPOCH",1},
-    {"jetski_epoch",7},
-    {"K01",2},
-    {"K02",2},
-    {"K03",2},
-    {"K04",2},
-    {"ebike_epoch",7},
-    {"mosquito_epoch",5},
-	{"C_Heli_Light_01_civil_EPOCH",5}
+   
+// Boats
+
+{"C_Boat_Civil_01_EPOCH",6},
+{"C_Boat_Civil_01_rescue_EPOCH",6},
+{"jetski_epoch",3},
+{"B_SDV_01_EPOCH",3},
+
+// Bikes
+
+{"C_Quadbike_01_EPOCH",4},
+
+// Cars
+
+{"C_Offroad_01_EPOCH",8},
+{"C_Hatchback_01_EPOCH",8},
+{"C_Hatchback_02_EPOCH",8},
+{"C_SUV_01_EPOCH",8},
+{"B_G_Offroad_01_armed_F",8},
+
+// Vans
+
+{"C_Van_01_box_EPOCH",6},
+{"C_Van_01_transport_EPOCH",6},
+{"B_G_Van_01_fuel_F",3},
+{"I_Truck_02_fuel_F",3},
+
+// Military Wheeled
+
+{"B_MRAP_01_EPOCH",8},
+{"O_MRAP_02_F",8},
+{"I_MRAP_03_F",8},
+
+// Military Trucks
+
+{"B_Truck_01_transport_EPOCH",1},
+{"B_Truck_01_covered_EPOCH",1},
+{"B_Truck_01_mover_EPOCH",1},
+{"B_Truck_01_box_EPOCH",1},
+{"O_Truck_02_covered_EPOCH",1},
+{"O_Truck_02_transport_EPOCH",2},
+{"O_Truck_03_covered_EPOCH",2},
+{"O_Truck_02_box_EPOCH",2},
+{"O_Truck_03_medical_F",6},
+{"O_Truck_03_covered_F",4},
+
+// Helicopters 
+         
+{"B_Heli_Light_01_EPOCH",8},              //MH9 Hummingbird
+{"I_Heli_Transport_02_EPOCH",4},          //Mohawk
+{"O_Heli_Transport_04_EPOCH",5},          //Taru Lift
+{"O_Heli_Transport_04_bench_EPOCH",3},    //Taru Bench
+{"O_Heli_Transport_04_box_EPOCH",3},      //Taru Transport
+{"O_Heli_Transport_04_covered_EPOCH",2},  //LB Wasp
+{"B_Heli_Transport_03_unarmed_EPOCH",2},  //Huron
+{"I_Heli_light_02_unarmed_F",2},          //Orca
+{"C_Heli_light_01_sheriff_F",1},          //LB Sherrif
+{"C_Heli_light_01_furious_F",1},          //LB Furious
+{"C_Heli_light_01_digital_F",1},          //LB digital
+{"C_Heli_light_01_speedy_F",1},           //LB speedy
+{"C_Heli_light_01_wasp_F",2},             //LB Wasp
+{"mosquito_epoch",4},                     //Coffin
+{"B_Heli_Transport_03_black_F",4},       //Huron Armed
+{"B_Heli_Transport_01_camo_F",4},        //Ghosthawk Armed
+{"C_Heli_Light_01_civil_EPOCH",5}        //new LB
+
 };
 
 // Traders
-taxRate = 0.1; // controls the price increase for purchases
-starterTraderItems[] = { { "ItemSodaBurst", "meatballs_epoch", "MortarBucket", "CinderBlocks", "VehicleRepair", "CircuitParts", "ItemCorrugated", "PartPlankPack", "ItemRock", "ItemRope", "ItemStick" }, { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 } }; // Starter Items for fresh spawned trader first array is classnames second is quantity.
-NPCSlotsLimit = 50; // Max number of traders static or dynamic. Warning! Higher the number lower performance.
+taxRate = 2.5; // controls the price increase for purchases
+starterTraderItems[] = { { "ItemSodaBurst", "meatballs_epoch", "MortarBucket", "CinderBlocks", "VehicleRepair", "CircuitParts", "ItemCorrugated", "PartPlankPack", "ItemRock", "ItemRope", "ItemStick" }, { 5, 5, 5, 10, 5, 5, 10, 5, 5, 5, 5} }; // Starter Items for fresh spawned trader first array is classnames second is quantity.
+NPCSlotsLimit = 30; // Max number of traders static or dynamic. Warning! Higher the number lower performance.
 forceStaticTraders = true; // disables traders moving from work to home
 
 // Markers
 showEarthQuakes = true; // show mineral viens caused by earthquakes
-showShippingContainers = true; // Show location of events based loots (plants, shipping container, Carnival)
+showShippingContainers = false; // Show location of events based loots (plants, shipping container, Carnival)
 SHOW_TRADERS = true; // Show locations of traders
 SHOW_JAMMERS = false; // Shows location of base jammers
 SHOW_BOATLOOT = true; // Shows the location of shipwreck loot
 DEBUG_VEH = false; // DEBUG ONLY used to debug spawing of vehicles
 
 // Hive Related
-expiresBuilding = "604800";  // expiration date in seconds for buildings
+expiresBuilding = "1209600";  // expiration date in seconds for buildings
 expiresPlayer = "2592000";  // expiration date in seconds for players
 expiresBank = "7776000";  // expiration date in seconds for players bank
-expiresVehicle = "604800";  // expiration date in seconds for vehicles
+expiresVehicle = "1209600";  // expiration date in seconds for vehicles
 expiresAIdata = "604800";  // expiration date in seconds for NPC Trader inventory
 hiveAsync = true; // true = asynchronous hive calls (non blocking), false = synchronous hive calls (blocking)
 
